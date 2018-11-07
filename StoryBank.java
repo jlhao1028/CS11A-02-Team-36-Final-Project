48ad2c8abd75197ad1fe8b79b5236cfa6efe7453
Jonah Sanders <jonahsanders@brandeis.edu>
2018-11-06 23:25:21 -0500

    File where we are going to put the stories

---
+++ StoryBank.java
@@ -0,0 +1,10 @@
+/**
+* The StoryBank class functions as the storage program of all narratives that
+the HickeyLibs program can call upon.
+* @author Jonah Sanders, Jiale Hao, Leah Fernandez
+* @version 1.0
+* @since 2018-11-6
+*/
+public class StoryBank{
+
+}
