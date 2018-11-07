eb94da970cfb7e1dc1bfcf7301d5fa595b2236e8
Jonah Sanders <jonahsanders@brandeis.edu>
2018-11-06 23:20:37 -0500

    Created basic framework for user Readme doc
    we'll do this at the end

---
+++ HickeyLibs.java
@@ -0,0 +1,12 @@
+/**
+* The HickeyLibs class generates a preset random storyline and allows the user to
+* fill in the blanks with nouns, adjectives, verbs, adverbs, and interjections to
+* create a humorous fun experience. At the conclusion of the program, it outputs
+* the new story with narration by Timothy Hickey and a friendly GUI.
+* @author Jonah Sanders, Jiale Hao, Leah Fernandez
+* @version 1.0
+* @since 2018-11-6
+*/
+public class HickeyLibs{
+
+}
